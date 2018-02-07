9a62d98af23f8fc881a1d279a451a895ca1e3c57
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-06 23:29:21 -0300

    Parcial 2

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,7 @@
 function Mostrar()
 {
-  
+  var producto
+  producto=prompt("el importes es:");
+  producto=document.getElementById('importeFinal').value
+  alert("El resultado es"+(producto*100)/21);
 }
