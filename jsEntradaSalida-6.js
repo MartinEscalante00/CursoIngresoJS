5ef600433faecf36b5df0925d2f852ef3adc6185
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-07 09:40:31 -0300

    ejercicio 6

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	numeroUno="2"
+	numeroDos="2"
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	var numeroUno=parseInt(2)
+	var numeroDos=parseInt(2)
+	resultado= numeroUno + numeroDos 
+
+
+	alert("La suma es"+resultado);
+
+
+}
+
