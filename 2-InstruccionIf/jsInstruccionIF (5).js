4fdf797df91353fbc701ef8b54a0aedd1756bbf3
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-07 20:09:31 -0300

    bien echo de otra forma

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+var edad;
+edad=document.getElementById('edad').value;
+if(edad<=12)
+{
+	alert("no es adolescente")
+	}if(edad>=18)
+	{
+		alert("no es adolescente")
+	}
+		
 
 
 }//FIN DE LA FUNCIÓN
