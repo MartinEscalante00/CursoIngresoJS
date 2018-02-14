d5f99ffeace3703ab1c27dbf0ebff4247dac3698
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-14 09:56:15 -0300

    bien echo

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+var estadoCivil;
+var edad;
+edad=document.getElementById('edad').value;
+estadoCivil=document.getElementById('estadoCivil').value;
+if(estadoCivil !="Soltero" )
+{
+	if(edad <18)
+	{
+		alert("Es muy pequeño para no ser soltero");
+	}
+}
 
 	
 
