08d020855dddcb8d469c1d8f202fab7f8837385c
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-14 12:23:27 -0300

    bien echo

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+			alert("Que comiences bien el año");
+			break;
+
+		case "Marzo":
+			alert("A clases");
+			break;
+
+		case "Diciembre":
+			alert("felices fiesta");
+			break;		
+	}
 
 
 
