e9ad057ed5fa17c316533da567e2ac75ceedf9b1
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-23 21:10:50 -0300

    bien echos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var numero1;
+	var importe
+	numero1=document.getElementById('importe').value;
+	importe=parseInt(numero1);
+
+	importe=((25/100)*(numero1));
+
+	importe=(numero1-importe);
+
+	document.getElementById("resultado").value= importe
 }
+
