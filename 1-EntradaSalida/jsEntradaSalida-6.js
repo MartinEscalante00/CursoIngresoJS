2ff6f12a69a830585afa516da6e64976703a5c39
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-23 21:09:57 -0300

    bien echo

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,51 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
 
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	suma= parseInt(numeroUno)+ parseInt(numeroDos);
+
+	alert("la suma es"+suma);
+
+
+	var numeroUno;
+	var numeroDos;
+	var resta;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	resta= parseInt(numeroUno) - parseInt(numeroDos);
+
+	alert("la suma es"+resta);
+
+
+	var numeroUno;
+	var numeroDos;
+	var multiplicacion;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	multiplicacion= parseInt(numeroUno) * parseInt(numeroDos);
+
+	alert("la suma es"+multiplicacion);
+
+	var numeroUno;
+	var numeroDos;
+	var division;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	division= parseInt(numeroUno) / parseInt(numeroDos);
+
+	alert("la suma es"+division);
 }
+
 
