396c0c8fc4b096641bc79341f4b113774860eaaa
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-26 20:43:17 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
-	alert('iteración while');
+	//definimos la variable
+	var contador;
+	//igualamos a contador a = 0
+	contador=0;
+	// creamos un while, tiene que ser menor a 10
+
+	while(contador<10)
+	{
+		// contador + 1, para que vaya en uno en uno
+		contador ++;
+		//br para que vaya en forma de columna
+		document.write("<br>"+contador);
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
+
+	while(clave !="utn750")
+	{
+		alert("error");
+		clave = prompt("ingrese el número clave.");
+	}
+	alert("muy bien");
 
 
 }//FIN DE LA FUNCIÓN
