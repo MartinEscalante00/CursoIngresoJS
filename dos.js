90769c7386e9b17a72e0bb3a6bf3d43dca107c53
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-02-27 16:20:57 -0300

    Add files via upload

---
+++ dos.js
@@ -0,0 +1,13 @@
+function Mostrar()
+{
+  var importe;
+  var importeFinal;
+  var precioFinal;
+
+  importe=prompt("ingrese importe");
+  importe=parseInt(importe)
+  importeFinal=importe*0.21
+  precioFinal= importe+importeFinal
+
+  document.getElementById('importeFinal').value=precioFinal;
+}
