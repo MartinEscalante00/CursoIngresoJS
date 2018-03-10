0e2fe139a361abf9a4ee8b0796221dd87fde4a82
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-03-10 11:33:19 -0300

    Add files via upload

--- cinco.js
+++ cinco.js
@@ -1,22 +1,71 @@
 function Mostrar()
 {
-	var dia;
-	dia=prompt("ingresar dia");
 
-	switch(dia)
+
+
+	var horas;
+	horas=prompt("ingrese hora");
+	switch(horas)
 	{
-		case "lunes":
-		case "martes":
-		case "miercoles":
-		case "jueves":
-		case "viernes":
-			alert("a trabajar");
+		
+
+		case "20":
+		case "21":
+		case "22":
+		case "23":
+		case "24":
+		case "0":
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		alert("es de noche")
+			if(horas<7)
+			{
+				alert("me voy a un bar");
+			}
 		break;
-		case"sabado":
-		case"domingo":
-			alert("es fin de semana");
+	
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+		alert("mañana")
+		if(horas<11)
+		{
+			alert("a desayunar");
+		}
 		break;
 
+		case "12":
+		case "13":
+		case "14":
+		case "15":
+		case "16":
+		case "17":
+		case "18":
+		case "19":
+		alert("tarde de mate");
+		break;
+
+		default:
+		alert("error");
+		break;
+
+
+
+
+
+		
 	}
-	
+
+
+
+
+
+
+
 }
