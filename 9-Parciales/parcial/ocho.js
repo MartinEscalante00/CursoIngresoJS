f361120d2df9799767dda4009c9bc98324c700bf
MartinEscalante00 <36193695+MartinEscalante00@users.noreply.github.com>
2018-03-10 12:57:38 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,40 @@
 function Mostrar()
+/**/
 {
+	var contador=0;
+	var respuesta="continuar";
+	var temperatura;
+
+	while(respuesta!="no")
+	{//inicio de llave
+
+		temperatura=prompt("ingrese temperatura");
+		temperatura=parseInt(temperatura);
+
+		while(temperatura<-100 || temperatura >100 || isNaN(temperatura))
+		{
+			temperatura=prompt("ingrese temperatura");
+			temperatura=parseInt(temperatura);
+		}
+
+		nombre=prompt("ingrese nombre");
+		while(! isNaN(nombre))
+		{
+			nombre=prompt("ingrese nombre");
+		}
+
+		sexo=prompt("ingrese sexo")
+
+
+
+
+
+
+		respuesta=prompt("ingrese no para salir");
+
+
+	}//fin de llave
+
+
 
 }
